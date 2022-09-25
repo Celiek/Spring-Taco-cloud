b6136a5fea54e05d98eb102e6ee4d4efe5d9b7e5
Celiek <bosKowski96@gmail.com>
2022-09-25 12:57:42 -0400

    Initial commit

---
+++ tacos/src/main/resources/data.sql
@@ -0,0 +1,26 @@
+delete from Taco_Order_Tacos;
+delete from Taco_Ingredients;
+delete from Taco;
+delete from Taco_Order;
+
+delete from Ingredient;
+insert into Ingredient(id , name type)
+            values('FLTO' , 'pszenna' , 'WRAP');
+insert into Ingredient(id , name type)
+            values('COTO' , 'kukurydziana' , 'WRAP');
+insert into Ingredient(id , name type)
+            values('GRBF' , 'mielona wołowina' , 'PROTEIN');
+insert into Ingredient(id , name type)
+            values('CARN' , 'kawałki mięsa' , 'PROTEIN');
+insert into Ingredient(id , name type)
+            values('TMTO' , 'pomidor' , 'VEGGIES');
+insert into Ingredient(id , name type)
+            values('LETC' , 'sałąta' , 'VEGGIES');
+insert into Ingredient(id , name type)
+            values('CHED' , 'cheddar' , 'CHEESE');
+insert into Ingredient(id , name type)
+            values('JACK' , 'ser monterey Jack' , 'CHEESE');
+insert into Ingredient(id , name type)
+            values('SLSA' , 'sos salsa' , 'SAUCE');
+insert into Ingredient(id , name type)
+            values('SRCR' , 'śmietana' , 'SAUCE');
